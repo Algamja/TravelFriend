93dcb3937e13ad532bf45837943ff3351cb94b0b
PROJECT <rlawkaud11@naver.com>
2020-10-09 21:00:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/travelfriend/SignUpEmailAndPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelfriend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpEmailAndPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_email_and_password)
+    }
+}
---
+++ app/src/main/java/com/example/travelfriend/SignUpUserBasicActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelfriend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpUserBasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_user_basic)
+    }
+}
