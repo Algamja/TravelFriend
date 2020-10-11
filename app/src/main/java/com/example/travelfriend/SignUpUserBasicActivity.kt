aa72da55aff26eb357be03b5071169790d8eb9bd
hyungjookim1 <suen66@naver.com>
2020-10-11 22:49:50 +0900

    [added]basic,detail을 수정

--- app/src/main/java/com/example/travelfriend/SignUpUserBasicActivity.kt
+++ app/src/main/java/com/example/travelfriend/SignUpUserBasicActivity.kt
@@ -1,12 +1,24 @@
 package com.example.travelfriend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up_user_basic.*
 
 class SignUpUserBasicActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_user_basic)
+
+        val input_name = sign_up_name_input.text.toString()
+        val input_phone = sign_up_phone_number_input.text.toString()
+
+        sign_up_second_next_button.setOnClickListener(){
+            val intent = Intent(this,Sign_up_user_detail::class.java)
+            intent.putExtra("name",input_name)
+            intent.putExtra("phone",input_phone)
+        startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/travelfriend/SignUpUserDetailActivity.kt
+++ app/src/main/java/com/example/travelfriend/SignUpUserDetailActivity.kt
@@ -1,12 +1,26 @@
 package com.example.travelfriend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up_user_detail.*
 
 class SignUpUserDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_user_detail)
+
+        val nickname = sign_up_nickname_input.text.toString()
+        val age = sign_up_age_input.text.toString()
+        val chk_gender = sign_up_gender_group.getCheckedRadioButtonId();
+
+        sign_up_done.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("nickname", nickname)
+            intent.putExtra("age", age)
+            intent.putExtra("gender", chk_gender)
+            startActivity(intent)
+        }
     }
 }
